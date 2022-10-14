0cb1110adb598444b01a78e329fbc549699cf2e8
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-14 16:42:49 -0300

    exclusions sonar-project

--- src/App.cy.js
+++ src/App.cy.js
@@ -17,7 +17,6 @@ it('component testing - shows the location host and path', () => {
     login: 'douglas',
     password: 'teste.cy.js',
   }
-  console.log(teste)
 
   cy.get('[data-cy=location]').contains('cy-test /App')
   cy.contains('[data-cy=location]', 'cy-test /App')
--- src/Location.js
+++ src/Location.js
@@ -1,88 +1,16 @@
-import { spawn } from 'prettier'
-
-console.log('After spawn()')
-
 export const getLocation = () => {
   const { hostname, pathname } = window.location
   console.log({ teste })
   return { hostname, pathname }
 }
 
-export const getLocation5 = () => {
+export const getLocation2 = () => {
   const { hostname, pathname } = window.location
   console.log({ teste })
   return { hostname, pathname }
 }
-export const getLocation6 = () => {
-  const { hostname, pathname } = window.location
-  console.log({ teste })
-  return { hostname, pathname }
-}
-
-export const teste = async (req, res) => {
-  const value = req?.query?.value
-  res.setHeader('Set-Cookie', value) // Noncompliant
-  res.cookie('connect.sid', value) // Noncompliant
-  alert('Unexpected Condition')
-  confirm('Teste')
-  spawn.teste('teste')
-}
-
 export const getLocation3 = () => {
   const { hostname, pathname } = window.location
   console.log({ teste })
   return { hostname, pathname }
-}
-
-export const getLocation4 = () => {
-  const { hostname, pathname } = window.location
-  console.log({ teste })
-  return { hostname, pathname }
-}
-
-function teste555(req, res) {
-  console.log(res)
-  const cmd = 'ls ' + req.query.arg
-  console.log(cmd)
-  // const client = new Client({
-  //   host: process?.env?.NODE_ENV,
-  //   port: 5334,
-  //   user: 'database-user',
-  //   password: 'secretpassword!!',
-  // })
-  // console.log(client)
-  // client.connect((err) => {
-  //   if (err) {
-  //     console.error('connection error', err.stack)
-  //   } else {
-  //     console.log('connected')
-  //   }
-  // })
-  // const out = process?.execSync(cmd) // Noncompliant: example of a command injection, req.query.arg = -la . ;cat /etc/passwd
-  // console.log(out)
-
-  return ''
-}
-
-function teste2(req, res) {
-  console.log(res)
-  const cmd = 'ls ' + req?.query?.arg
-  const out = onabort().execSync(cmd) // Noncompliant: example of a command injection, req.query.arg = -la . ;cat /etc/passwd
-  console.log(out)
-}
-
-export const getLocation2 = () => {
-  const { hostname, pathname } = window.location
-  console.log({ teste })
-  teste2(null, null)
-  testeX(null, null)
-  teste555(null, null)
-  return { hostname, pathname }
-}
-
-async function testeX(req, res) {
-  const value = req?.query?.value
-
-  res?.setHeader('Set-Cookie', value) // Noncompliant
-  res?.cookie('connect.sid', value) // Noncompliant
 }
