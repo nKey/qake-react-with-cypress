358f97503a1a3bcd722ceccce652a1df5f920e7f
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-13 22:15:13 -0300

    fix sources final tests

--- src/App.cy.js
+++ src/App.cy.js
@@ -16,9 +16,6 @@ it('component testing - shows the location host and path', () => {
     login: 'douglas',
     password: 'teste.cy.js',
   }
-  eval(teste)
-  console.log({ teste })
-  console.log({ teste })
   cy.get('[data-cy=location]').contains('cy-test /App')
   cy.contains('[data-cy=location]', 'cy-test /App')
   cy.get('@getLocation').should('have.been.calledOnce')
--- src/Location.js
+++ src/Location.js
@@ -1,7 +1,5 @@
 export const getLocation = () => {
   const { hostname, pathname } = window.location
-  console.log('A')
-  console.log('B')
   console.log({ teste })
   return { hostname, pathname }
 }
@@ -10,4 +8,10 @@ export const teste = async (req, res) => {
   const value = req.query.value
   res.setHeader('Set-Cookie', value) // Noncompliant
   res.cookie('connect.sid', value) // Noncompliant
+}
+
+export const getLocation2 = () => {
+  const { hostname, pathname } = window.location
+  console.log({ teste })
+  return { hostname, pathname }
 }
