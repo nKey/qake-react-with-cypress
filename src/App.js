210ca812c8033dc40932fa41c5f54ccd126e11e9
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-14 00:54:21 -0300

    fix sources final tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { getLocation } from './Location'
+import process from 'process'
 
 function App() {
   const { hostname, pathname } = getLocation()
