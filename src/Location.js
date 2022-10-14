989e09acce95a43432825ea7dc8bbfad1099203e
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-13 22:12:35 -0300

    fix sources final tests

--- src/Location.js
+++ src/Location.js
@@ -2,7 +2,12 @@ export const getLocation = () => {
   const { hostname, pathname } = window.location
   console.log('A')
   console.log('B')
-  console.log('C')
   console.log({ teste })
   return { hostname, pathname }
+}
+
+export const teste = async (req, res) => {
+  const value = req.query.value
+  res.setHeader('Set-Cookie', value) // Noncompliant
+  res.cookie('connect.sid', value) // Noncompliant
 }
