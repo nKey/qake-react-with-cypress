8f3c51afd26ad8269b17ad13dfe13beb2f1babd5
Douglas Adriano Queiroz <douglasqueiroz@Mac-mini-de-Douglas.local>
2022-10-17 12:20:24 -0300

    fix: coverage and duplication lines

--- src/Location.js
+++ src/Location.js
@@ -1,16 +1,4 @@
 export const getLocation = () => {
   const { hostname, pathname } = window.location
-  console.log("teste a")
   return { hostname, pathname }
 }
-export const getLocation2 = () => {
-  const { hostname, pathname } = window.location
-  console.log("teste b")
-  return { hostname, pathname }
-}
-export const getLocation3 = () => {
-  const { hostname, pathname } = window.location
-  console.log("teste c")
-  return { hostname, pathname }
-}
-
