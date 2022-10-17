009c52c657a1803bd485c7f784e8c98a94f8bbc3
douglasqueiroznkey <81320101+douglasqueiroznkey@users.noreply.github.com>
2022-10-17 11:23:18 -0300

    Update Location.js

--- src/Location.js
+++ src/Location.js
@@ -1,8 +1,16 @@
 export const getLocation = () => {
   const { hostname, pathname } = window.location
+  console.log("teste a")
   return { hostname, pathname }
 }
 export const getLocation2 = () => {
   const { hostname, pathname } = window.location
+  console.log("teste b")
   return { hostname, pathname }
 }
+export const getLocation3 = () => {
+  const { hostname, pathname } = window.location
+  console.log("teste c")
+  return { hostname, pathname }
+}
+
