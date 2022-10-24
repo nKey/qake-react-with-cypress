ba168604cfc33bec190def94d09381ecc5a5e737
José Malty Soares <jose.malty@nkey.com.br>
2022-10-24 12:24:44 -0300

    fix: tests

--- src/App.js
+++ src/App.js
@@ -18,19 +18,4 @@ function App() {
   )
 }
 
-function App() {
-  const { hostname, pathname } = getLocation()
-  console.log(hostname)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p data-cy="location">
-          {hostname} {pathname}{' '}
-        </p>
-      </header>
-    </div>
-  )
-}
-
 export default App
