a89a09eeb5707689fa04c8074791c39ca12febab
José Malty Soares <jose.malty@nkey.com.br>
2022-10-24 12:22:48 -0300

    feat: break sonar tests

--- src/App.js
+++ src/App.js
@@ -18,4 +18,19 @@ function App() {
   )
 }
 
+function App() {
+  const { hostname, pathname } = getLocation()
+  console.log(hostname)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p data-cy="location">
+          {hostname} {pathname}{' '}
+        </p>
+      </header>
+    </div>
+  )
+}
+
 export default App
